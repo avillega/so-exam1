d26d83045f44974c6335778e78776c1598c07281
Andres Villegas <villegas.pelaez.andres@gmail.com>
2017-03-05 21:49:04 -0500

    Add files via upload

---
+++ A00239620/scripPunto2.sh
@@ -0,0 +1,5 @@
+while read ip
+do
+    sshpass -p 'operativos' ssh -o StrictHostKeyChecking=no operativos@$ip "curl http:///www.gutenberg.org/files/54240/54240-0.txt >> /tmp/libroDescargado.txt"
+done < ipsFile.txt
+
